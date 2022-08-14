57364f9a3c148c3722bdb7a9b4b500d3fe484cb5
loftol <tkfkddl59323@naver.com>
2022-08-14 14:20:14 +0900

    new start

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,4 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "plugin:react/recommended",
-    "standard",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint", "prettier"],
-  rules: {
-    "prettier/prettier": ["error", { endOfLine: "auto" }],
-  },
+  root: true,
+  extends: '@react-native-community',
 };
