b342a707a73b67722fc58e7ea85efe423c431f17
loftol <tkfkddl59323@naver.com>
2022-10-05 23:18:48 +0900

    Update mainComponents.tsx

--- src/components/mainComponents.tsx
+++ src/components/mainComponents.tsx
@@ -2,6 +2,6 @@
 
 export {default as MapBoardContainer} from './mapBoard/mapBoardContainer';
 export {default as MenuContainer} from './menu/menuContainer';
-export {default as ToiletInfoContainer} from './toilentInfo/toiletInfoContainer';
+export {default as ToiletInfoContainer} from './toiletInfo/toiletInfoContainer';
 export {default as SearchBar} from './searchBar/searchBar';
 export {default as ReviewPage} from './toiletInfo/ToiletReview/ReviewPage';
