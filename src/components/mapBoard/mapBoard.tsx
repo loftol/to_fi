97e2d3b6332b3db0066826a52810b2bebabca865
loftol <tkfkddl59323@naver.com>
2022-10-14 01:17:39 +0900

    genaralization

--- src/components/mapBoard/mapBoard.tsx
+++ src/components/mapBoard/mapBoard.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import {View} from 'react-native';
 import {WebView} from 'react-native-webview';
 import {useDispatch} from 'react-redux';
-import {change} from '../../common/toiletIdReducer';
 import localInfo from '../../../localInfo';
-
-
 import {addData} from '../../common/toiletDataReducer';
 
 export default function mapBoard() {
